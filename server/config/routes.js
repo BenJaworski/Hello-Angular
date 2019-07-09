ff41a63bb8cebc5a97cf1d3b8998ddd36ea67c22
Ben_J <benzjamin@live.com>
2019-07-09 13:36:32 -0700

    Restful Tasks

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const controller = require('../controllers/tasks.js');
 
 module.exports = (app)=>{
-    app.get('/', controller.index)
-    app.get('/:_id', controller.search)
+    app.get('/tasks', controller.index)
+    app.get('/task/:_id', controller.search)
     app.post('/new', controller.create)
-    app.post('/:_id', controller.update)
-    app.post('/remove/:_id', controller.delete)
+    app.put('/:_id', controller.update)
+    app.delete('/remove/:_id', controller.delete)
 }
